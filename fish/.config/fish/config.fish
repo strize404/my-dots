9e2c790003b4a4ff234f78dc0e546508ff196aa3
strize07 <strizeop@gmail.com>
2026-03-25 16:22:45 +0530

    nix fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,38 +2,32 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias tree='tree --charset utf-8'
+set -g fish_key_bindings fish_vi_key_bindings
+set -g fish_greeting
+
+alias ns="nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history"
+alias nd='nix develop --command fish'
 alias inv='nvim $(fzf -m --preview="bat --color=always {}")'
 alias fzf='fzf -m --preview="bat --color=always {}"'
-alias y='yazi'
-alias nv='nvim'
-alias gc='git clone'
-alias lg='lazygit'
 alias lt='eza -T -L1 --icons'
 alias ltt='eza -T -L2 --icons'
 alias lt3='eza -T -L3 --icons'
-# alias ns="nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history"
-# alias nd 'nix develop --command fish'
-
-set -g fish_key_bindings fish_vi_key_bindings
-set -g fish_greeting
+alias c='clear'
+alias y='yazi'
+alias nv='nvim'
+alias lg='lazygit'
+alias gc='git clone'
 
 # disable ctrl i tab Commands
 function fish_user_key_bindings
     bind \ci ''
 end
+atuin init --disable-up-arrow fish | source
 
 export EDITOR=nvim
 export VISUAL=nvim
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
-starship init fish | source
-atuin init --disable-up-arrow fish | source
-zoxide init fish | source
-set -gx PATH ~/.npm-global/bin $PATH
 # Start tmux
-tmux
-# Delay fastfetch by 2 seconds
-sleep 1 && fastfetch
-
-#add bun
-set -gx PATH $PATH ~/.bun/bin
+# tmux
